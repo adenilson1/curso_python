d7b3df68f710f3948a39981a6268e489acacb605
adenilson1 <adenilson99@hotmail.com>
2024-09-30 07:56:03 -0300

    Comandos intermediarios

---
+++ intermadiario/aula37.py
@@ -0,0 +1,11 @@
+from sys import path
+
+import aula37_package
+from aula37_package import modulo
+from aula37_package.modulo import*
+import aula37_package.modulo
+
+print(soma_do_modulo(1,2))
+print(aula37_package.modulo.soma_do_modulo(1,3))
+print(variavel)
+print(nova_variavel)
