ff976dcf33db4d490b32e2d8004d6a9ceb171bf2
adenilson1 <adenilson99@hotmail.com>
2024-10-07 11:35:58 -0300

    Comandos intermediarios

--- intermadiario/aula40.py
+++ intermadiario/aula40.py
@@ -5,7 +5,6 @@ def fora(x):
     
     def dentro():
         print(locals())
-        print(dentro.__code__.co_freevars)
         return a
     return dentro
 
@@ -16,3 +15,22 @@ dentro2 = fora(20)
 print(dentro1())
 print()
 print(dentro2())
+
+print(50*'-')
+print()
+
+def concatenar(string_inicial):
+    valor_final = string_inicial
+
+    def interna(valor_a_concatenar=''):
+        nonlocal valor_final # fora da função, o python busca em outro local
+        valor_final += valor_a_concatenar
+        return valor_final
+    return interna
+
+c = concatenar('a')
+print(c('b'))
+print(c('c'))
+print(c('d'))
+final = c()
+print(final)
