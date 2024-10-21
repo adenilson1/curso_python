68c50ab27a566b30285225921687a8e3bc352e22
adenilson1 <adenilson99@hotmail.com>
2024-10-21 12:33:02 -0300

    Comandos intermediarios

---
+++ intermadiario/aula51.py
@@ -0,0 +1,11 @@
+"""
+Funções recursivsa e recursividade
+-funções que podem se chamar de volta
+- uteis para dividir prblemas grandes em partes menores
+Toda função recursiva deve ser:
+-Um problema que possa ser dividida em partes menores
+-Um caso recursivo que resolve o pequeno problema 
+-Um caso base que para a recursão 
+-fatorial - n! = 5 * 4 * 3 * 2 * 1 = 120
+"""
+
