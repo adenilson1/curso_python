22e83d302f1cbf5e66c029b26f3837129bf13f74
adenilson1 <adenilson99@hotmail.com>
2024-10-22 15:36:14 -0300

    Comandos intermediarios

---
+++ intermadiario/aula53.py
@@ -0,0 +1,13 @@
+"""
+Ambientes virtuais em Python (venv)
+Um ambiente virtual carrega toda a instalação
+do python para uma pasta no caminho escolhido.
+Ao ativar um ambiente virtual, a instalação do 
+ambiente virtual serar usada.
+venv é o módulo que vamos usar para 
+criar ambientes virtuais.
+Você pode dar o nome que preferiri para ambiente virtual,
+mas os mais comuns são:
+venv env .venv .env
+"""
+
