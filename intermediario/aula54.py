0ad3c787038c458157021fabaf7e108a51e31722
adenilson1 <adenilson99@hotmail.com>
2024-10-28 11:58:01 -0300

    Comandos intermediarios

--- intermediario/aula54.py
+++ intermediario/aula54.py
@@ -5,6 +5,7 @@ um arquivo em Python (ele pode ou nao existir)
 Modos:
 r (leitura), w (escrita), x (criação), a (escrever ao final), b(binario)
 t (modo texto), + (leitura e escrita)
+Context manager - with (abre e fecha)
 Metodos uteis:
 write, read, (escrever e ler)
 writelines (escrever varias linhas)
@@ -18,3 +19,15 @@ Vamos falar mais sovre o modulo json, mas:
 json.dump = Gera um arquivo json
 json.load
 """
+
+caminho_arquivo = '/home/ad/Área de trabalho/python/intermediario/'
+caminho_arquivo += 'aula54.txt'
+print(caminho_arquivo)
+
+#arquivo = open(caminho_arquivo, 'w') 
+#
+#arquivo.close()
+
+with open (caminho_arquivo, 'w') as arquivo:
+    print('Olá mundo!!!!')
+    print('Arquivo vai ser fechado')
