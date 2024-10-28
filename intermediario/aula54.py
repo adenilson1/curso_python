3dcd1b532b15326520b0c3817fbb6433f04554a1
adenilson1 <adenilson99@hotmail.com>
2024-10-28 11:29:13 -0300

    Comandos intermediarios

---
+++ intermediario/aula54.py
@@ -0,0 +1,20 @@
+"""
+Criando argquivos com Python
+Usamos a função open para abrir
+um arquivo em Python (ele pode ou nao existir)
+Modos:
+r (leitura), w (escrita), x (criação), a (escrever ao final), b(binario)
+t (modo texto), + (leitura e escrita)
+Metodos uteis:
+write, read, (escrever e ler)
+writelines (escrever varias linhas)
+seek (move o cursor)
+readline (ler linha)
+readlines (ler linhas)
+Vamos falar mais sobre o modulo os, mas:
+os.remove ou unlink - apaga o arquivo
+os.rename - troca o nome o move o arquivo
+Vamos falar mais sovre o modulo json, mas:
+json.dump = Gera um arquivo json
+json.load
+"""
