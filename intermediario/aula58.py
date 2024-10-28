884372b8f038d39a36cdb385b7c19c1fef5fbf74
adenilson1 <adenilson99@hotmail.com>
2024-10-28 20:16:52 -0300

    Comandos intermediarios

--- intermediario/aula58.py
+++ intermediario/aula58.py
@@ -1,3 +1,6 @@
+
+import json
+
 pessoa = {
     'nome': 'Adenilson',
     'sobrenome': 'Diniz',
@@ -9,4 +12,19 @@ pessoa = {
     'numeros_preferidos': (2,4,6,8,10),
     'dev': True,
     'nada': None,
+
 }
+
+with open('aula58.json','w',encoding='utf8') as arquivo:
+    json.dump(pessoa,
+              arquivo,
+              ensure_ascii=False,
+              indent=2,
+)
+
+
+with open('aula58.json','r', encoding='utf8') as arquivo:
+    pessoa = json.load(arquivo)
+    #print(pessoa)
+    #print(type(pessoa))
+    print(pessoa['nome'])
