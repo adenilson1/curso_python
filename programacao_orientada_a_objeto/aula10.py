b0f1a6cea6b9cadd09f8ae81693390839a19605e
adenilson1 <adenilson99@hotmail.com>
2024-11-06 18:32:33 -0300

    @staticmethod (métodos estáticos) são inúteis em Python)

---
+++ programacao_orientada_a_objeto/aula10.py
@@ -0,0 +1,6 @@
+# @staticmethod (metodos estaticos) são inúteis em PendingDeprecationWarning
+# Métodos estáticos são métodos que estão denstro da 
+# classe, mas não tem acesso ao self nem ao cls.
+# Em resumo, são funções que existem dentro da sua 
+# classe
+
