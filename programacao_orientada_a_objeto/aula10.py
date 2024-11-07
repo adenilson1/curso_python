0cb355d68dee9c8cce2c6a927d91b096e4f62ff6
adenilson1 <adenilson99@hotmail.com>
2024-11-07 19:20:15 -0300

    @staticmethod (métodos estáticos) são inúteis em Python)

--- programacao_orientada_a_objeto/aula10.py
+++ programacao_orientada_a_objeto/aula10.py
@@ -4,3 +4,17 @@
 # Em resumo, são funções que existem dentro da sua 
 # classe
 
+class Classe:
+    @staticmethod
+    def funcao_que_esta_dentro_na_classe(*args, **kwargs):
+        print('Oi', args, kwargs)
+
+#mesma coisa de uma funcao
+def funcao(*args, **kwargs):
+    print('Oi', args, kwargs)
+
+c1 = Classe()
+c1.funcao_que_esta_dentro_na_classe(1,2,3)
+funcao(1,2,3)
+Classe.funcao_que_esta_dentro_na_classe(nomeado=1)
+funcao(nomeado=1)
