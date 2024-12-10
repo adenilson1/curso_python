08dbfbcb68a15eb36f8a907b0cfa162c4f61fba9
adenilson1 <adenilson99@hotmail.com>
2024-12-10 17:10:10 -0300

    Herança Múltipla-Python Orientado a Objeto

--- programacao_orientada_a_objeto/aula23.py
+++ programacao_orientada_a_objeto/aula23.py
@@ -14,3 +14,32 @@
 # Para saber a ordem de chamada dos médotos
 # Use o método de classes Classe.mro()
 # Ou o atributo __mro__ (Dunder - Double Underscore)
+
+class A:
+    ...
+
+    def quem_sou(self):
+        print('A')
+
+class B(A):
+    ...
+
+ #   def quem_sou(self):
+ #       print('B')
+
+class C(A):
+    ...
+
+    def quem_sou(self):
+        print('C')
+
+class D(B,C):
+    ...
+
+#    def quem_sou(self):
+#        print('D')
+
+d = D()
+d.quem_sou()
+#print(D.__mro__)
+print(D.mro())
