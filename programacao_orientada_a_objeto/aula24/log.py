4ca2e3e7181a8d9111e07b110d03ad256737a51b
adenilson1 <adenilson99@hotmail.com>
2025-02-10 19:02:52 -0300

    (parte 2). Mixins, Abstração e a união de tudo

--- programacao_orientada_a_objeto/aula24/log.py
+++ programacao_orientada_a_objeto/aula24/log.py
@@ -1,15 +1,30 @@
 #Abstração
+#Herança - é um 
 class Log:
-    def log(self, msg): # assinatura do metodo
+    def _log(self, msg): # assinatura do metodo
         raise NotImplementedError('Implemente o médoto log')
 
 
-class LogFileMixin(Log):
-    def log(self, msg):
+    def log_error(self, msg):
+       return self._log(f'Erro: {msg}')
+    
+    def log_success(self, msg):
+       return self._log(f'Success: {msg}')
+
+
+class LogFileMexin(Log):
+    def _log(self, msg):
         print(msg)
 
 
+
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
+
+
 if __name__ == '__main__':
-    l = LogFileMixin()
-    l.log('Qualquer coisa')
+    l = LogPrintMixin()
+    l.log_error('Qualquer coisa')
+    l.log_success('Que Legal')
    
