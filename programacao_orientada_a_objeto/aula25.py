7215a728c896193e2def02adca4f412d2984c636
adenilson1 <adenilson99@hotmail.com>
2025-02-18 10:24:32 -0300

    Classes abstratas - Abstract Base Class (abc)

--- programacao_orientada_a_objeto/aula25.py
+++ programacao_orientada_a_objeto/aula25.py
@@ -15,3 +15,22 @@
 # @staticmethod e @method como abstratos, para isso
 # use @abstractmethod como decorato mais interno.
 
+from abc import ABC, abstractmethod
+
+class Log(ABC):
+    @abstractmethod
+    def _log(self, msg): # assinatura do metodo
+       ...
+
+    def log_error(self, msg):
+       return self._log(f'Error: {msg}')
+    
+    def log_success(self, msg):
+       return self._log(f'Success: {msg}')
+    
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
+
+l=LogPrintMixin()
+l.log_error('Oi')
