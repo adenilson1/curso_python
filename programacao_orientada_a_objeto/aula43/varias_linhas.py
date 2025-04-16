b38a13a1cbf948e74e568d90ba177585eefa2533
adenilson1 <adenilson99@hotmail.com>
2025-04-16 11:53:38 -0300

    DocStrings de varias linhas(DOcumentação)

---
+++ programacao_orientada_a_objeto/aula43/varias_linhas.py
@@ -0,0 +1,6 @@
+"""O que seu módulo faz
+
+Lorem ipsum dolor sit amet.Ut sequi enim vel similique consequuntur
+non earum laboriosam est fugit quibusdam.Aut dolore velit nam voluptatem 
+omnis ut doloribus sunt a tempore itaque quo blanditiisobcaecati.
+"""
