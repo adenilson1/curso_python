43c70f5ec325a5170540832e89e6cb4e6901618f
adenilson1 <adenilson99@hotmail.com>
2025-04-20 11:49:47 -0300

    Exercicio

---
+++ programacao_orientada_a_objeto/exercicio03.py
@@ -0,0 +1,10 @@
+"""
+Exercicio com Abstração, Herança, Encapsulamento e
+Polimorfismo
+Criar um sistema bancário (extremamente simples)
+que tem clientes, contas e um banco.
+A ideia é que o cliente tenha uma conta (poupança 
+ou corrente) e que possa sacar/depositar nessa
+conta. COntas corrente tem um limite extra
+
+"""
