4cab21dc71d108acb5667640bc3698fecd6beaef
adenilson1 <adenilson99@hotmail.com>
2025-04-22 17:38:58 -0300

    Criando a classe ContaPoupanca

--- programacao_orientada_a_objeto/exercicio03/contas.py
+++ programacao_orientada_a_objeto/exercicio03/contas.py
@@ -13,9 +13,30 @@ class Conta(abc.ABC):
 
     def depositar(self, valor):
         self.saldo += valor
-        self.detalhes(f'DÉPOSITO {valor}')
+        self.detalhes(f' (DÉPOSITO {valor})')
 
     
     def detalhes(self, msg=''):
         print(f'O seu saldo é {self.saldo:.2f}{msg}')
+        print('----------------------------')
 
+
+
+class ContaPoupanca(Conta):
+    def sacar(self, valor):
+        valor_pos_sacar = self.saldo - valor
+
+        if valor_pos_sacar >= 0:
+            self.saldo -= valor
+            self.detalhes(f' (SAQUE {valor})')
+            return self.saldo
+        
+        print('Não foi possível sacar o valor desejado')
+        self.detalhes(f' (SAQUE NEGADO {valor})')
+
+if __name__ == '__main__':
+    cp1 = ContaPoupanca(111,222,0)
+    cp1.sacar(1)
+    cp1.depositar(1)
+    cp1.sacar(1)
+    cp1.sacar(1)
