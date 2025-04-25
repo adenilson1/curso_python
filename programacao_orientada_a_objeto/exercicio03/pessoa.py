0d7d85832cda34fa4640847b340b05a042bf1658
adenilson1 <adenilson99@hotmail.com>
2025-04-25 20:58:39 -0300

    criação da classe cliente

--- programacao_orientada_a_objeto/exercicio03/pessoa.py
+++ programacao_orientada_a_objeto/exercicio03/pessoa.py
@@ -1,3 +1,6 @@
+import contas
+
+
 class Pessoa:
     def __init__(self, nome: str, idade: int) -> None:
         self.nome = nome
@@ -18,3 +21,26 @@ class Pessoa:
     @idade.setter
     def idade(self, idade: int):
         self._idade = idade
+
+    def __repr__(self) -> str:
+        class_name = type(self).__name__
+        attr = f"({self.nome!r}, {self.idade!r})"
+        return f"{class_name}{attr}"
+
+
+class Cliente(Pessoa):
+    def __init__(self, nome: str, idade: int) -> None:
+        super().__init__(nome, idade)
+        self.conta: contas.Conta | None = None
+
+
+if __name__ == "__main__":
+    c1 = Cliente("Luiz", 30)
+    c1.conta = contas.ContaCorrente(111, 222, 0, 0)
+    print(c1)
+    print(c1.conta)
+
+    c2 = Cliente("Maria", 10)
+    c2.conta = contas.ContaPoupanca(112, 223, 100)
+    print(c2)
+    print(c2.conta)
