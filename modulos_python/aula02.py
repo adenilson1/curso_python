113120be72890adecafe69864bc78837315df30a
adenilson1 <adenilson99@hotmail.com>
2025-05-04 11:38:28 -0300

    Data e hora atual

---
+++ modulos_python/aula02.py
@@ -0,0 +1,33 @@
+"""
+Criando datas com módulos datetime
+datetime (ano, mês, dia)
+datatime (ano, mês, dia, horas, minutos, segundos)
+datetime.striptime('DATA', 'FORMATO')
+datatime.now()
+Instalando o pytz
+pip instal pytz types-pytz
+"""
+
+from datetime import datetime
+
+# from pytz import timezone
+
+# data = datetime.now(timezone("Asia/Tokyo"))
+
+# data = datetime(2022, 4, 20, 7, 23, tzinfo=timezone("Asia/Tokyo"))
+# print(data)
+
+
+# data_str_data = "2022/04/20 07:49:23"
+# data_str_data_brazil = "20/04/2022"
+# data_str_formato = "%Y/%m/%d %H:%M:%S"
+# data_str_formato_brazil = "%d/%m/%Y"
+
+# data = datetime.strptime(data_str_data, data_str_formato)
+# data_brazil = datetime.strptime(data_str_data_brazil, data_str_formato_brazil)
+
+# print(data_brazil)
+
+data = datetime.now()
+print(data.timestamp())  # isso esta na base de dados
+print(datetime.fromtimestamp(1746369090))
