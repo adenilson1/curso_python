099bb3f3f8e68a7f506f0bc64b88f42b85fc9391
adenilson1 <adenilson99@hotmail.com>
2025-05-09 16:40:12 -0300

    módulo para interação com o sistema

---
+++ modulos_python/aual07.py
@@ -0,0 +1,22 @@
+"""
+O módulo 'os' para interação como sistema
+O módulo 'os' fornece funções par ainteragir como o
+sistema operacional
+Por exemplo.: o módulo os.path contém funções para
+trabalhar com caminhos de arquivos e a função os.listdir()
+pode ser usada para lista os arquivos em um diretorio
+O módulo os.system() permite executar comandos do
+sistema operacional a partir do seu código
+"""
+
+import os
+
+os.system("clear")
+os.system("echo 'Hello World'")
+
+print("a" * 80)
+print("a" * 80)
+print("a" * 80)
+print("a" * 80)
+print("a" * 80)
+print("a" * 80)
