aa934cee60a2a5a3be93176f51f2c1514bea224b
adenilson1 <adenilson99@hotmail.com>
2025-06-18 10:23:04 -0300

    O que é JSON

---
+++ modulos_python/aula14.py
@@ -0,0 +1,24 @@
+# O que é json ?
+# JSON - JavaScript object Notation (extesão .json)
+# É uma estrutura de dados que permite a serialização
+# de objetos em texto simples para facilitar a transmissão de
+# dados através da rede, APIs web ou outras meiso de comunicação.
+# O JSON suporta os seguintes tipod de dados:
+# Números: podem ser inteiros ou com ponto flutuante, com 42 ou 3.14
+# Strings: são cadeias de caracteres, como "Olá, mundo !" ou "12345"
+#     As strubgs deven ser envolvidas por aspas duplas
+# Booleanos: são os valores vedadeiros(True) ou falso (False)
+# Arrays: são listas ordenadas de valores, como [1,2,3] ou
+#     ["Oi", "Olá", "Bom dia"]
+# objectos: são conjuntos de pares chave/valor -> {"nome": "João", "idade": 30}
+# null: é um valor especial uqe represnta ausência de valor
+
+# Ao converter de Python para JSON
+# Python ---------- JSON
+# dict ---------- object
+# list, tuple ---------- array
+# str ---------- string
+# int, float ---------- number
+# True ---------- true
+# False ---------- false
+# None ---------- null
