47a8ee1d64798c18a948ffcd0a3db8440f4ae8c8
adenilson1 <adenilson99@hotmail.com>
2025-07-04 10:02:06 -0300

    CSV(Comma Separated Values - Valores separados por virgula

---
+++ modulos_python/aula17.py
@@ -0,0 +1,19 @@
+# CSV (Comma Separated Values)
+# É um formato de arquivo que armazena dados em forma de tabela, onde cada
+# linha representa uma linha da tabela e as colunas são separadas por vírgulas
+# Ele é amplamente utilizado para transferir dados entre sistemas de diferentes
+# plataformas, como por exemplo, para importar ou exportar dados para uma
+# planilha (Google Sheets, Excel, LibreOffice Calc) ou para uma base de dados
+# Um arquivo CSV geralmente tem a extensão ".csv" e pode ser aberto em um
+# editor de texto ou em uma planilha eletrônica.
+# Um exemplo de um arquivo CSV pode ser:
+# Nome, Idade, Endereço
+# Luiz Otávio, 32, "Av bRASIL, 21, Centro"
+# João da Silva, 55, "Rua 22, 44, Nova Era"
+# A primeira linha do arquivo define os nomes das colunas da, enquanto as
+# linhas seguintes contêm os valores das linhas, separados por virgula.
+# Regras simples do CSV:
+# Separe os valores das colunas com um delimitador unico (,)
+# Cada registro deve estar em linha
+# Não deixar linhas ou espaços sobrando
+# Use o caracter de escape ("") quando o delimitador aparecer.
