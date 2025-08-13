54404d6cbd24c133b292cc7019b9eda75ece3cf6
adenilson1 <adenilson99@hotmail.com>
2025-08-13 18:09:30 -0300

    (parte 2) - COmpactando / Descomapctando arquivos com zipfile.ZipFile

--- modulos_python/aula28.py
+++ modulos_python/aula28.py
@@ -8,7 +8,7 @@ from zipfile import ZipFile
 # Caminhos
 CAMINHO_RAIZ = Path(__file__).parent
 CAMINHO_ZIP_DIR = CAMINHO_RAIZ / "aula_28_diretorio_zip"
-CAMINHO_COMPACTODO = CAMINHO_RAIZ / "aula_28_compactdao.zip"
+CAMINHO_COMPACTODO = CAMINHO_RAIZ / "aula_28_compactado.zip"
 CAMINHO_DESCOMPACTADO = CAMINHO_RAIZ / "aula28_descompactado"
 
 shutil.rmtree(CAMINHO_ZIP_DIR, ignore_errors=True)
@@ -30,3 +30,18 @@ def criar_caminho(qtd: int, zip_dir: Path):
 
 
 criar_caminho(10, CAMINHO_ZIP_DIR)
+# Criando um zip e adicionando arquivos
+with ZipFile(CAMINHO_COMPACTODO, "w") as zip:
+    for root, dirs, files in os.walk(CAMINHO_ZIP_DIR):
+        for file in files:
+            # print(file)
+            zip.write(os.path.join(root, file), file)
+
+# Lendo arquivos de um zip
+with ZipFile(CAMINHO_COMPACTODO, "r") as zip:
+    for arquivo in zip.namelist():
+        print(arquivo)
+
+# Extrando arquivos de um zip
+with ZipFile(CAMINHO_COMPACTODO, "r") as zip:
+    zip.extractall(CAMINHO_DESCOMPACTADO)
